e183d221c2757c2d8ca900136d122379ecffe6b3
MoovSpace <moovspace@gmail.com>
2020-06-05 21:44:41 +0200

    Get bearer token

--- src/App/Router/HttpHeader.php
+++ src/App/Router/HttpHeader.php
@@ -5,16 +5,15 @@ class HttpHeader
 {
     static function BearerToken()
     {
-        return self::GetHeader();
+        return trim(str_replace('Bearer', '', self::GetHeader()));
     }
 
-    protected static function GetHeader($name = 'Authorization')
+    static function GetHeader($name = 'Authorization')
     {
         $arr = getallheaders();
         if(!empty($arr[$name]))
         {
-            $token = trim($arr[$name]);
-            return str_replace('Bearer ','',$token);
+            return $arr[$name];
         }
         return '';
     }
